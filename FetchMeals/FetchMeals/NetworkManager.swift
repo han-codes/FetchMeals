efa3c5be1260b71962acefaa8e01f63cf63485cb
Han Kim <h.kim@fetchrewards.com>
2022-03-24 14:47:48 -0500

    Add networking to fetch desserts and display them in a table view

--- FetchMeals/FetchMeals/NetworkManager.swift
+++ FetchMeals/FetchMeals/NetworkManager.swift
@@ -7,7 +7,85 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case invalidURL
+}
+
+struct Meal: Decodable {
+    let name: String
+    let thumbnailURL: URL?
+    let id: Int
+
+    enum CodingKeys: String, CodingKey {
+        case name = "strMeal"
+        case thumbnailURLString = "strMealThumb"
+        case id = "idMeal"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        name = try container.decode(String.self, forKey: .name)
+
+        let rawThumbnailURLString = try container.decode(String.self, forKey: .thumbnailURLString)
+        thumbnailURL = URL(string: rawThumbnailURLString)
+
+        let idString = try container.decode(String.self, forKey: .id)
+        id = Int(idString) ?? 0
+    }
+}
+struct RootMeal: Decodable {
+
+    enum RootCodingKeys: String, CodingKey {
+        case meals
+    }
+
+    let meals: [Meal]
+
+    init(from decoder: Decoder) throws {
+        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
+        meals = try rootContainer.decode([Meal].self, forKey: .meals)
+    }
+}
+
+struct MealDetail: Decodable {
+
+}
+
 class NetworkManager {
 
-    
+    func fetchDesserts() async -> [Meal] {
+        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert") else {
+            return []
+        }
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: urlRequest, delegate: nil)
+            let meals = try JSONDecoder().decode(RootMeal.self, from: data).meals
+            return meals
+        } catch {
+            return []
+        }
+    }
+
+    func fetchMealDetails(forMealId id: String) async throws -> [MealDetail] {
+
+        guard let url = URL(string: "https://www.themealdb.com/api/json/v1/1/lookup.php?i=\(id)") else {
+            return []
+        }
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+
+        do {
+            let (data, _) = try await URLSession.shared.data(for: urlRequest, delegate: nil)
+            let mealDetails = try JSONDecoder().decode([MealDetail].self, from: data)
+            return mealDetails
+        } catch {
+            return []
+        }
+    }
 }
--- FetchMeals/FetchMeals/ViewController.swift
+++ FetchMeals/FetchMeals/ViewController.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    var desserts: [Meal]?
+
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+
+        setUpViews()
+
+        Task.init(priority: .background) {
+            let desserts = await NetworkManager().fetchDesserts()
+            self.desserts = desserts
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
 
+    private func setUpViews() {
 
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return desserts?.count ??  0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+
+        var content = cell.defaultContentConfiguration()
+        content.text = desserts?[indexPath.row].name
+
+        cell.contentConfiguration = content
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
